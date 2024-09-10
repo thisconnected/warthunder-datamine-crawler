8f4429b54dae72ba4348cc42833ff2ed9425ef6c
Patryk Kaniewski <p7tryk@gmail.com>
2024-09-10 20:15:24 +0200

    transposed csv for nicer output

--- main.py
+++ main.py
@@ -12,13 +12,25 @@ missile_preset = [
     ]
 
 missile_list = []
+missile_list.append(Missile.missile_description())
+missile_list.append(Missile.missile_units())
 
 for missile in missile_preset:
     new_missile = Missile(missile)
     missile_list.append(new_missile.missile_calculate())
 
+
+transposed = list(map(list, zip(*missile_list)))
+
+
+# with open("test.csv", "w") as csvfile:
+#     writer = csv.writer(csvfile, delimiter=";")
+#     writer.writerow(Missile.missile_description())
+#     for missile in missile_list:
+#         print(missile)
+#         writer.writerow(missile)
+
 with open("test.csv", "w") as csvfile:
     writer = csv.writer(csvfile, delimiter=";")
-    writer.writerow(Missile.missile_description())
-    for missile in missile_list:
-        writer.writerow(missile)
+    for row in transposed:
+        writer.writerow(row)
